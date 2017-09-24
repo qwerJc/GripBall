d6b7ce61513344ea3605690baf6e84aced7edfae
JiaChen_Home <951311987@qq.com>
2017-09-25 00:18:05 +0800

    0

---
+++ GripBall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GripBall
+//
+//  Created by 贾辰 on 2017/9/24.
+//  Copyright © 2017年 贾辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
